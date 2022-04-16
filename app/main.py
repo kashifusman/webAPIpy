5aafc4c23c22b7a931126ebf924101f81220101a
Kashif Usman <kusman@contour-software.com>
2022-04-17 01:24:45 +0500

    Reverted all changes 1,2,3 This is final

--- app/main.py
+++ app/main.py
@@ -26,8 +26,8 @@ app.add_middleware(
 
 app.include_router(post.router)
 app.include_router(user.router)
-# app.include_router(auth.router)
-# app.include_router(vote.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def root():
