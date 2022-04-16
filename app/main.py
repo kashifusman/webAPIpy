b2387b118e46ee5bcae55814e51a3115c0fce3e1
Kashif Usman <kusman@contour-software.com>
2022-04-17 01:08:24 +0500

    will revert these changes back 1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 # to fetching the column names and converting into dict object
-# from .routers import post,user, auth, vote
+from .routers import post,user, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -24,7 +24,7 @@ app.add_middleware(
 )
 
 
-# app.include_router(post.router)
+app.include_router(post.router)
 # app.include_router(user.router)
 # app.include_router(auth.router)
 # app.include_router(vote.router)
