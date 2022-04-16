98b3c7234c474145eccb21aa485a238d1af71392
Kashif Usman <kusman@contour-software.com>
2022-04-17 01:09:13 +0500

    will revert these changes back 2

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
 
 
 app.include_router(post.router)
-# app.include_router(user.router)
+app.include_router(user.router)
 # app.include_router(auth.router)
 # app.include_router(vote.router)
 
